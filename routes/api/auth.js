817b8d465533a63f026d1fbbf854427a182e291d
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2023-01-28 21:35:07 +0200

    login/feature
    create auth/login for login user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,10 +3,14 @@ const express = require("express");
 const ctrl = require("../../controllers/auth");
 const ctrlWrapper = require("../../helpers/ctrlWrapper");
 const { validator } = require("../../middlewares/validationMidlewares");
-const { registerSchema } = require("../../schemas/auth");
+const { registerSchema, loginSchema } = require("../../schemas/auth");
 
 const router = express.Router();
 
 router.post("/register", validator(registerSchema), ctrlWrapper(ctrl.register));
+
+router.post("/login", validator(loginSchema), ctrlWrapper(ctrl.login));
+
+router.post("logout");
 
 module.exports = router;
