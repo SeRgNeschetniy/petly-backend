a1a65f365da9e5fb3ddad621a4e71f5a4c6dbad0
SeRgNeschetniy <xdsergx@gmail.com>
2023-01-30 12:53:31 +0200

    add googleAuth

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,10 +2,12 @@ const register = require("./register");
 const login = require("./login");
 const logout = require("./logout");
 const getUserInfo = require("./getUserInfo");
+const google = require("./google");
 
 module.exports = {
   register,
   login,
   logout,
   getUserInfo,
+  google,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,8 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 const ctrlWrapper = require("../../helpers/ctrlWrapper");
-const authenticate = require("../../middlewares/authenticate");
-const { validator } = require("../../middlewares/validationMidlewares");
+const { validator, authenticate, passport } = require("../../middlewares");
 const { registerSchema, loginSchema } = require("../../schemas/auth");
 
 const router = express.Router();
@@ -15,5 +14,18 @@ router.post("/login", validator(loginSchema), ctrlWrapper(ctrl.login));
 router.post("/logout", authenticate, ctrlWrapper(ctrl.logout));
 
 router.get("/current", authenticate, ctrlWrapper(ctrl.getUserInfo));
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  ctrlWrapper(ctrl.google)
+);
 
 module.exports = router;
