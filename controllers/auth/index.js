e60a625c285bae9ddef1bdeac39fe8c76d6a99e8
ArtemRe <RechembeiArtem@gmail.com>
2023-01-30 06:24:25 +0200

    FIx news folder

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,12 +2,10 @@ const register = require('./register');
 const login = require('./login');
 const logout = require('./logout');
 const getUserInfo = require('./getUserInfo');
-const getNews = require('./getNews');
 
 module.exports = {
   register,
   login,
   logout,
   getUserInfo,
-  getNews,
 };
--- routes/api/news.js
+++ routes/api/news.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const getNews = require('../../controllers/auth/getNews');
+const getNews = require('../../controllers/news/getNews');
 const ctrlWrapper = require('../../helpers/ctrlWrapper');
 
 const router = express.Router();
