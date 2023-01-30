dc4c34568a4e121180cc52bbfe91d661a07aba13
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2023-01-30 13:23:27 +0200

    feature/updateUserInfo
    add auth/update

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,10 +2,12 @@ const register = require("./register");
 const login = require("./login");
 const logout = require("./logout");
 const getUserInfo = require("./getUserInfo");
+const updateUserInfo = require("./updateUserInfo");
 
 module.exports = {
   register,
   login,
   logout,
   getUserInfo,
+  updateUserInfo,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,11 @@ const ctrl = require("../../controllers/auth");
 const ctrlWrapper = require("../../helpers/ctrlWrapper");
 const authenticate = require("../../middlewares/authenticate");
 const { validator } = require("../../middlewares/validationMidlewares");
-const { registerSchema, loginSchema } = require("../../schemas/auth");
+const {
+  registerSchema,
+  loginSchema,
+  updateUserSchema,
+} = require("../../schemas/auth");
 
 const router = express.Router();
 
@@ -15,5 +19,12 @@ router.post("/login", validator(loginSchema), ctrlWrapper(ctrl.login));
 router.post("/logout", authenticate, ctrlWrapper(ctrl.logout));
 
 router.get("/current", authenticate, ctrlWrapper(ctrl.getUserInfo));
+
+router.patch(
+  "/update",
+  authenticate,
+  validator(updateUserSchema),
+  ctrlWrapper(ctrl.updateUserInfo)
+);
 
 module.exports = router;
