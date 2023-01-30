d1d46d878060d1c70943249258a57dfc1aae3711
Ohleh <oleg.kolischuk@gmail.com>
2023-01-30 18:50:04 +0200

    add cosmetix to shema pet

--- controllers/pets/removePet.js
+++ controllers/pets/removePet.js
@@ -7,13 +7,13 @@ const removePet = async (req, res) => {
   if (!pet) {
     return res
       .status(404)
-      .json({ message: "This pet doesn`t exist, nothing to remove" });
+      .json({ message: "The pet you want to remove, is already removed." });
   } else {
     const removedPet = await Pets.findByIdAndDelete(petId);
     if (!removedPet) {
       return res
         .status(404)
-        .json({ message: "This pet doesn`t exist, nothing to remove" });
+        .json({ message: "This pet doesn`t exist, nothing to remove." });
     }
     res.status(200).json({
       message: `Pet ${pet.name} ${pet.breed} successfully deleted`,
