5b8262da70eb7c83489afbead441a5e6ca733128
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-01 00:08:29 +0200

    bugfix

--- routes/api/news.js
+++ routes/api/news.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const getNews = require('../../controllers/news/getNews');
-const ctrlWrapper = require('../../helpers/ctrlWrapper');
+const express = require("express");
+const getNews = require("../../controllers/news/getNews");
+const ctrlWrapper = require("../../helpers/ctrlWrapper");
 
 const router = express.Router();
 
-router.get('/news', ctrlWrapper(getNews));
+router.get("/news", ctrlWrapper(getNews));
 
 module.exports = router;
