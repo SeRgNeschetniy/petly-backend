ae0fa4b06b88cc23fb19e0086dfba01541d3f0ec
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-02 15:09:16 +0200

    Update news.js

--- routes/api/news.js
+++ routes/api/news.js
@@ -4,8 +4,6 @@ const ctrlWrapper = require("../../helpers/ctrlWrapper");
 
 const router = express.Router();
 
-
-router.get('/', ctrlWrapper(getNews));
-
+router.get("/", ctrlWrapper(getNews));
 
 module.exports = router;
