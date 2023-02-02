d3f2fdd562499b4b2a3895abe1d1b6b380ead03b
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-02 11:48:01 +0200

    bugfix

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const ctrlUsers = require("../../controllers/auth");
+const ctrlUsers = require("../../controllers/users");
 const ctrlNotices = require("../../controllers/notices");
 
 const ctrlWrapper = require("../../helpers/ctrlWrapper");
