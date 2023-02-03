51f1281a393d8e34eaac2e7efe826bbfdf976f1e
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-03 21:45:54 +0200

    Update google.js

--- controllers/users/google.js
+++ controllers/users/google.js
@@ -15,7 +15,7 @@ const google = async (req, res) => {
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
-  res.redirect(`${BASE_URL}:${PORT}?token=${token}`);
+  res.redirect(`http://localhost:3000/petly-frontend/login?token=${token}`);
 };
 
 module.exports = google;
