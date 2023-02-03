b813b5997bc3655d0b370ccdb74cfe562e850ba4
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2023-02-03 20:45:59 +0200

    fix/user-Joi
    fix user validation Joi and add regexp

--- schemas/auth.js
+++ schemas/auth.js
@@ -1,30 +1,38 @@
 const Joi = require("joi");
 
+const emailRegexp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const passwordRegexp = /^[a-zA-Z0-9а-яА-Я]+$/;
+const nameRegexp = /^[a-zA-Z-\s]+$/;
+const cityRegexp = /^[a-zA-Z]+,?\s[a-zA-Z]+$/;
+const phoneRegexp = /^\+380\d{9}$/;
+const birthdayRegexp =
+  /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+
 const registerSchema = Joi.object({
-  email: Joi.string().required(),
-  password: Joi.string().min(7).max(32).required(),
-  name: Joi.string().required(),
-  city: Joi.string().required(),
-  phone: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().pattern(passwordRegexp).min(7).max(32).required(),
+  name: Joi.string().pattern(nameRegexp).required(),
+  city: Joi.string().pattern(cityRegexp).required(),
+  phone: Joi.string().pattern(phoneRegexp, "numbers").required(),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().required(),
-  password: Joi.string().min(7).max(32).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().pattern(passwordRegexp).min(7).max(32).required(),
 });
 
 const updateUserSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  birthday: Joi.string().required(),
-  phone: Joi.string().required(),
-  city: Joi.string().required(),
+  name: Joi.string().pattern(nameRegexp),
+  email: Joi.string().pattern(emailRegexp),
+  birthday: Joi.string().pattern(birthdayRegexp),
+  phone: Joi.string().pattern(phoneRegexp, "numbers"),
+  city: Joi.string().pattern(cityRegexp),
 });
 
 const emailSchema = Joi.object({
-  email: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
 });
-
 
 module.exports = {
   registerSchema,
