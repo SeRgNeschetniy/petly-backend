b90780310ba735cc1be0550dde9dd13dd2befec2
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-06 12:01:46 +0200

    Update getUserInfo.js

--- controllers/users/getUserInfo.js
+++ controllers/users/getUserInfo.js
@@ -1,6 +1,9 @@
+const User = require("../../models/user");
+
 const getUserInfo = async (req, res) => {
-  const { email, name, city, phone, birthday, avatarURL, _id } = req.user;
-  res.json({ email, name, city, phone, birthday, avatarURL, _id });
+  const { _id } = req.user;
+  const user = await User.find(_id, { password: 0, token: 0, refreshToken: 0 });
+  res.status(200).json(user);
 };
 
 module.exports = getUserInfo;
