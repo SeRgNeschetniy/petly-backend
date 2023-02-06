d703b8be9ff85f4cc6526aa81514b20306793704
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-06 17:57:56 +0200

    Update addNoticeToFavorites.js

--- controllers/notices/addNoticeToFavorites.js
+++ controllers/notices/addNoticeToFavorites.js
@@ -1,18 +1,12 @@
 const User = require("../../models/user");
 const RequestError = require("../../helpers/requestError");
+const { Notice } = require("../../models/notice");
 
 const addNoticeToFavorites = async (req, res) => {
   const { noticeId } = req.params;
   const { id: userId } = req.user;
 
-  const user = await User.updateOne(
-    { _id: userId },
-    { $addToSet: { favorites: noticeId } }
-  );
-
-  if (!user) {
-    throw RequestError("Unable to add Notice to favorites.");
-  }
+  await User.updateOne({ _id: userId }, { $addToSet: { favorites: noticeId } });
 
   res.status(200).json("Notice was added to favorites.");
 };
