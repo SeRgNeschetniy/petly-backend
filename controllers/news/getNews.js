f402bb1588201ff9dbefa8518b50f7746686e91d
SeRgNeschetniy <xdsergx@gmail.com>
2023-02-08 11:38:50 +0200

    Update getNews.js

--- controllers/news/getNews.js
+++ controllers/news/getNews.js
@@ -1,7 +1,7 @@
 const New = require("../../models/news");
 
 const getNews = async (req, res) => {
-  const result = await New.find();
+  const result = await New.find().sort({ date: -1 });
 
   res.status(200).json(result);
 };
